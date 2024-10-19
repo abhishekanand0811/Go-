08b77a986fec315e5b79618182c8c4b24afa4bea
unknown <abhishekanand08112002@gmail.com>
2024-10-19 09:37:18 +0530

    second commit

--- arrays.go
+++ arrays.go
@@ -1,30 +1,33 @@
 package main
-import ("fmt")
 
-func main(){
+import (
+	"fmt"
+)
+
+func main() {
 
 	//arrays with defined length
-	var arr1 = [3]int{1,2,3}
-	arr2 := [5]int{3,4,5,6,7}
-	fmt.Println(arr1)
+	var arr11 = [3]int{1, 2, 3}
+	arr2 := [5]int{3, 4, 5, 6, 7}
+	fmt.Println(arr11)
 	fmt.Println(arr2)
 
 	//arrays with undefined length
-	var arr3 = [...]int{1,2,3}
-	arr4 := [...]int{4,5,6,7,8}
+	var arr3 = [...]int{1, 2, 3}
+	arr4 := [...]int{4, 5, 6, 7, 8}
 	fmt.Println(arr3)
 	fmt.Println(arr4)
 
 	//change elemetns of an array
-	prices :=[3]int{10,20,30}
-	prices[2]=50
+	prices := [3]int{10, 20, 30}
+	prices[2] = 50
 	fmt.Println(prices)
 
 	//array initialization
-	arr5 :=[5]int{}//not initialized
-	arr6 :=[5]int{1,2}//partially initialized
-	arr7 :=[5]int{1,2,3,4,5}//fully initialized
-	arr8 :=[5]int{1:10,2:40}
+	arr5 := [5]int{}              //not initialized
+	arr6 := [5]int{1, 2}          //partially initialized
+	arr7 := [5]int{1, 2, 3, 4, 5} //fully initialized
+	arr8 := [5]int{1: 10, 2: 40}
 	fmt.Println(arr5)
 	fmt.Println(arr6)
 	fmt.Println(arr7)
